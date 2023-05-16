920bf2bb457ddb5d2dee79c0fa0f35e3b3923963
RomanRomanenko <roman.romanenko@aterise.com>
2023-05-16 20:24:17 +0300

    added background

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,12 +7,15 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
+        self.bg_immage = pygame.image.load('images/background.webp')
 
     def run_game(self):
         while True:
             for event in pygame.event.get():
                 if event == pygame.QUIT:
                     sys.exit()
+
+            self.screen.blit(self.bg_immage, (0, 0))
 
             pygame.display.flip()
 
