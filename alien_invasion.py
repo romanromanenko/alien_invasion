d8e31beb83eb7b1c08eb0578b5e40b2aaf079fef
RomanRomanenko <roman.romanenko@aterise.com>
2023-05-16 20:36:51 +0300

    added settings module and short refactoring for alien_invasion

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,13 +1,17 @@
 import sys
 import pygame
+from settings import Settings
 
 class AlienInvasion:
     def __init__(self):
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((
+            self.settings.screen_width,
+            self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-        self.bg_immage = pygame.image.load('images/background.webp')
+
 
     def run_game(self):
         while True:
@@ -15,7 +19,7 @@ class AlienInvasion:
                 if event == pygame.QUIT:
                     sys.exit()
 
-            self.screen.blit(self.bg_immage, (0, 0))
+            self.screen.blit(self.settings.bg_immage, (0, 0))
 
             pygame.display.flip()
 
