b9596aa5f4b12ad7989270f7508276825a637570
RomanRomanenko <roman.romanenko@aterise.com>
2023-05-20 22:53:24 +0300

    test

--- alien_invasion.py
+++ alien_invasion.py
@@ -13,10 +13,7 @@ class AlienInvasion:
         self.settings = Settings()
 
         self.screen = pygame.display.set_mode(
-            (
-                self.settings.screen_width,
-                self.settings.screen_height
-            )
+            (self.settings.screen_width,self.settings.screen_height)
         )
         pygame.display.set_caption("Alien Invasion")
         self.stats = GameStats(self)
